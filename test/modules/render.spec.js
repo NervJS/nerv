a946d842f93a741f730210cc16c0adfbb2040257
luckyadam <weitaozsh@gmail.com>
2017-07-28 01:49:14 +0800

    fix:test

--- test/modules/render.spec.js
+++ test/modules/render.spec.js
@@ -588,23 +588,6 @@ describe('render()', function () {
     expect(scratch.innerHTML, 'root re-render 2').to.equal(`<div><b>alt</b><a>foo</a><c>baz</c><b>bat</b></div>`)
   })
 
-  /* global xit */
-  ;('HTMLDataListElement' in window ? it : xit)('should allow <input list /> to pass through as an attribute', () => {
-    render((
-      <div>
-        <input type='range' min='0' max='100' list='steplist' />
-        <datalist id='steplist'>
-          <option>0</option>
-          <option>50</option>
-          <option>100</option>
-        </datalist>
-      </div>
-    ), scratch)
-
-    let html = scratch.firstElementChild.firstElementChild.outerHTML
-    expect(sortAttributes(html)).to.equal(sortAttributes('<input type="range" min="0" max="100" list="steplist">'))
-  })
-
   it('should not execute append operation when child is at last', (done) => {
     let input
     class TodoList extends Component {
