a240f8ac39030699954b30bec072dd17f7f64266
yuche <i@yuche.me>
2017-08-24 15:09:32 +0800

    chore: add eslint ignore

--- src/lib/render-queue.js
+++ src/lib/render-queue.js
@@ -1,6 +1,7 @@
 import nextTick from './util/next-tick'
 import { updateComponent } from './lifecycle'
-const items = []
+
+let items = []
 
 export function enqueueRender (component) {
   if (!component._dirty && (component._dirty = true) && items.push(component) === 1) {
@@ -10,8 +11,8 @@ export function enqueueRender (component) {
 
 export function rerender () {
   let p
-  const list = items.concat()
-  items.length = 0
+  const list = items
+  items = []
   while ((p = list.pop())) {
     if (p._dirty) {
       updateComponent(p)
