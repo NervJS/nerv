befade87e0fae0f4b1838d42e719151a76bf1931
yuche <i@yuche.me>
2017-08-24 16:38:03 +0800

    merge

--- test/modules/util.spec.js
+++ test/modules/util.spec.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import * as Util from '../../src/lib/util/index'
-=======
 import * as Util from '../../src/lib/util'
->>>>>>> 030b0e486d83aaa316be52334f2c484ae3517a6f
 import shallowEqual from '../../src/lib/util/shallow-equal'
 
 describe('Util', () => {
@@ -11,16 +7,11 @@ describe('Util', () => {
     const b = 'b'
     const c = () => {}
     const d = {}
-<<<<<<< HEAD
-    const f = [1, 2, 3]
-    const h = false
-=======
     const e = new Date()
     const f = [1, 2, 3]
     const g = /\s/
     const h = false
     const i = new Error('error')
->>>>>>> 030b0e486d83aaa316be52334f2c484ae3517a6f
 
     expect(Util.isNumber(a)).to.be.true
     expect(Util.isString(b)).to.be.true
@@ -30,15 +21,12 @@ describe('Util', () => {
     expect(Util.isBoolean(h)).to.be.true
   })
 
-<<<<<<< HEAD
-=======
   it('isEmptyObject', () => {
     expect(Util.isEmptyObject({a: 1})).not.to.be.true
     expect(Util.isEmptyObject({})).to.be.true
     expect(Util.isEmptyObject(null)).to.be.true
     expect(Util.isEmptyObject(undefined)).to.be.true
   })
->>>>>>> 030b0e486d83aaa316be52334f2c484ae3517a6f
 
   it('isNative', () => {
     function a () {}
@@ -48,11 +36,7 @@ describe('Util', () => {
   })
 
   it('extend && clone', () => {
-<<<<<<< HEAD
-    let a = { a: 1, b: 2 }
-=======
     const a = { a: 1, b: 2 }
->>>>>>> 030b0e486d83aaa316be52334f2c484ae3517a6f
     Util.extend(a, { c: 1 })
     expect(a).to.have.property('a', 1)
     expect(a).to.have.property('b', 2)
@@ -65,11 +49,7 @@ describe('Util', () => {
     Util.extend(a, { a: { aa: 1, bb: 2 } })
     expect(a).to.deep.equal({a: { aa: 1, bb: 2 }, b: 2, c: 1})
 
-<<<<<<< HEAD
-    let b = Util.clone(a)
-=======
     const b = Util.clone(a)
->>>>>>> 030b0e486d83aaa316be52334f2c484ae3517a6f
     expect(b).to.deep.equal({a: { aa: 1, bb: 2 }, b: 2, c: 1})
     b.b = 10
     expect(b).to.have.property('b', 10)
@@ -77,16 +57,6 @@ describe('Util', () => {
   })
 
   it('shallowEqual', () => {
-<<<<<<< HEAD
-    let a = {a: 1}
-    let b = {a: 2}
-    let c = {a: 1}
-    let d = {a: 1, v: []}
-    let e = {a: 1, v: []}
-    const arr1 = []
-    let f = {a: 1, v: arr1}
-    let g = {a: 1, v: arr1}
-=======
     const a = {a: 1}
     const b = {a: 2}
     const c = {a: 1}
@@ -95,14 +65,9 @@ describe('Util', () => {
     const arr1 = []
     const f = {a: 1, v: arr1}
     const g = {a: 1, v: arr1}
->>>>>>> 030b0e486d83aaa316be52334f2c484ae3517a6f
     expect(shallowEqual(a, b)).not.to.be.true
     expect(shallowEqual(a, c)).to.be.true
     expect(shallowEqual(d, e)).not.to.be.true
     expect(shallowEqual(f, g)).to.be.true
   })
-<<<<<<< HEAD
-
-=======
->>>>>>> 030b0e486d83aaa316be52334f2c484ae3517a6f
 })
