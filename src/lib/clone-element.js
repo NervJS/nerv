555cdb3b31b422f7ca446515c918a4be2148c6ec
luckyadam <weitaozsh@gmail.com>
2017-09-05 19:59:55 +0800

    fix:cloneElement append children

--- src/lib/clone-element.js
+++ src/lib/clone-element.js
@@ -2,21 +2,13 @@ import createElement from './create-element'
 import { extend, clone } from './util'
 
 export default function cloneElement (vnode, props) {
-  let properties = clone(vnode.props)
-  if (properties.attributes) {
-    properties = extend(properties, properties.attributes)
-    delete properties.attributes
-  }
-  properties = extend(properties, props)
-  if (vnode.key) {
-    properties.key = vnode.key
-  }
+  props = extend(clone(vnode.props), props)
   if (vnode.namespace) {
-    properties.namespace = vnode.namespace
+    props.namespace = vnode.namespace
   }
   return createElement(
     vnode.tagName,
-    properties,
-    arguments.length > 2 ? [].slice.call(arguments, 2) : vnode.props.children
+    props,
+    arguments.length > 2 ? [].slice.call(arguments, 2) : vnode.children
   )
 }
