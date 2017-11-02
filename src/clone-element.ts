701a7b0d3b110f2dc77978668c5cdd5dd08b8f28
luckyadam <weitaozsh@gmail.com>
2017-11-02 22:59:34 +0800

    fix:clone text element

--- src/clone-element.ts
+++ src/clone-element.ts
@@ -1,6 +1,10 @@
 import createElement from './create-element'
 import { extend, clone } from './util'
+import { isVText } from './vdom/vnode/types'
 export default function cloneElement (vnode, props): any {
+  if (isVText(vnode)) {
+    return vnode
+  }
   const properties = extend(clone(vnode.props), props)
   if (vnode.namespace) {
     properties.namespace = vnode.namespace
