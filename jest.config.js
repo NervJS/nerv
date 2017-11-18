31009eab359e3f8e69600380800e97e91e647a7e
yuche <i@yuche.me>
2017-11-18 21:26:26 +0800

    test: fix tagName typo

--- jest.config.js
+++ jest.config.js
@@ -31,7 +31,7 @@ module.exports = {
   rootDir: __dirname,
   testMatch: [
     // '<rootDir>/packages/nerv/__tests__/componentDidCatch.spec.js'
-    '<rootDir>/packages/nerv/__tests__/dom.spec.js'
+    '<rootDir>/packages/nerv/__tests__/refs.spec.js'
     // '<rootDir>/packages/**/__tests__/**/*spec.js'
   ],
   transform: {
