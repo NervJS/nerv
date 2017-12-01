96abadd0134e1dc3e63b8b58ff51ed3403932d86
yuche <i@yuche.me>
2017-12-01 17:53:21 +0800

    test: use promise in karma config

--- karma.conf.js
+++ karma.conf.js
@@ -83,9 +83,10 @@ module.exports = function (config) {
     // list of files / patterns to load in the browser
     files: [
       'browsers/karma.js',
+      './node_modules/es6-promise/dist/es6-promise.auto.min.js',
       './node_modules/es5-polyfill/dist/polyfill.js',
-      // 'browsers/ie8.js',
-      // 'browsers/polyfill.js',
+      'browsers/ie8.js',
+      'browsers/polyfill.js',
       'packages/nerv/__tests__/event.spec.js',
       'packages/*/__tests__/**/*spec.js?(x)'
     ],
