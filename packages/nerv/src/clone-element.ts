9a693d5742890d01cc612839e39aa2391aaa15ad
luckyadam <weitaozsh@gmail.com>
2017-12-28 15:25:47 +0800

    fix:cloneElement with recursive copy children

--- packages/nerv/src/clone-element.ts
+++ packages/nerv/src/clone-element.ts
@@ -1,18 +1,51 @@
 import createElement from './create-element'
-import { extend, clone } from 'nerv-utils'
-import { isVText } from 'nerv-shared'
+import createVText from './vdom/create-vtext'
+import { extend, clone, isArray, isString } from 'nerv-utils'
+import { isVText, isVNode } from 'nerv-shared'
 
-export default function cloneElement (vnode, props, ...children): any {
+export default function cloneElement (vnode, props?: object, ...children): any {
   if (isVText(vnode)) {
+    vnode.dom = null
     return vnode
   }
-  const properties = extend(clone(vnode.props), props)
+  if (isString(vnode)) {
+    return createVText(vnode)
+  }
+  const properties = clone(extend(clone(vnode.props), props))
   if (vnode.namespace) {
     properties.namespace = vnode.namespace
   }
-  return createElement(
-    vnode.type,
-    properties,
-    arguments.length > 2 ? [].slice.call(arguments, 2) : vnode.children
-  )
+  let childrenTmp =
+    (arguments.length > 2 ?
+      [].slice.call(arguments, 2) :
+      vnode.children || properties.children) || []
+  if (childrenTmp.length) {
+    if (childrenTmp.length === 1) {
+      childrenTmp = children[0]
+    }
+  }
+  if (isArray(vnode)) {
+    return vnode.map((item) => {
+      return cloneElement(item)
+    })
+  }
+  const newVNode = createElement(vnode.type, properties)
+  if (!newVNode) {
+    return newVNode
+  }
+  if (isArray(childrenTmp)) {
+    const _children = childrenTmp.map((child) => {
+      return cloneElement(child, child.props)
+    })
+    if (isVNode(newVNode)) {
+      newVNode.children = _children
+    }
+    newVNode.props.children = _children
+  } else if (childrenTmp) {
+    if (isVNode(newVNode)) {
+      newVNode.children = childrenTmp
+    }
+    newVNode.props.children = cloneElement(childrenTmp, childrenTmp.props)
+  }
+  return newVNode
 }
