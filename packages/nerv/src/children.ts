5060bade4937ef571daed432ff455dbcc071a4c6
yuche <i@yuche.me>
2018-01-03 15:54:08 +0800

    fix: flatten children before call Children.forEach etc.

--- packages/nerv/src/children.ts
+++ packages/nerv/src/children.ts
@@ -1,8 +1,19 @@
 import { isArray } from 'nerv-utils'
-import { isNullOrUndef, VirtualChildren, EMPTY_CHILDREN } from 'nerv-shared'
+import {
+  isNullOrUndef,
+  VirtualChildren,
+  EMPTY_CHILDREN,
+  isInvalid
+} from 'nerv-shared'
+
+export type IterateFn = (
+  value: VirtualChildren | any,
+  index: number,
+  array: Array<VirtualChildren | any>
+) => any
 
 export const Children = {
-  map (children: Array<VirtualChildren | any>, fn: any, ctx: any): any[] {
+  map (children: Array<VirtualChildren | any>, fn: IterateFn, ctx: any): any[] {
     if (isNullOrUndef(children)) {
       return children
     }
@@ -14,7 +25,7 @@ export const Children = {
   },
   forEach (
     children: Array<VirtualChildren | any>,
-    fn: Function,
+    fn: IterateFn,
     ctx: any
   ): void {
     if (isNullOrUndef(children)) {
@@ -25,7 +36,9 @@ export const Children = {
       fn = fn.bind(ctx)
     }
     for (let i = 0, len = children.length; i < len; i++) {
-      fn(children[i], i, children)
+      const child = isInvalid(children[i]) ? null : children[i]
+
+      fn(child, i, children)
     }
   },
   count (children: Array<VirtualChildren | any>): number {
@@ -45,6 +58,25 @@ export const Children = {
     if (isNullOrUndef(children)) {
       return []
     }
-    return isArray(children) ? children : EMPTY_CHILDREN.concat(children)
+    if (isArray(children)) {
+      const result = []
+
+      flatten(children, result)
+
+      return result
+    }
+    return EMPTY_CHILDREN.concat(children)
   }
+}
+
+function flatten (arr, result) {
+  for (let i = 0, len = arr.length; i < len; i++) {
+    const value = arr[i]
+    if (isArray(value)) {
+      flatten(value, result)
+    } else {
+      result.push(value)
+    }
+  }
+  return result
 }
